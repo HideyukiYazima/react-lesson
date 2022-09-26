14164736e4f27f3f2d6524b5e7d4230905e42f9c
Yazima <yazima04200@gmail.com>
2022-09-26 17:53:30 +0900

    ボタン切り替え追加

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {name: 'にんじゃわんこ'};
+    
+  }
+
   render() {
     const name = 'にんじゃわんこ';
     const imgUrl = 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/ninjawanko.png';
@@ -10,8 +16,13 @@ class App extends React.Component {
         <h1>{name}</h1>
         <h1>Hello World</h1>
         <p>一緒にReactを学びましょう！</p>
+
         <img src={imgUrl} />
-        
+
+        <h1>こんにちは、{this.state.name}さん！</h1>
+        <button onClick={() => {this.setState({name: 'ひつじ仙人'})}}>ひつじ仙人</button>
+        <button onClick={() => {this.setState({name: 'にんじゃわんこ'})}}>にんじゃわんこ</button>
+
       </div>
     );
   }
