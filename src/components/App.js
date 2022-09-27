7daa4ff3d1bb27fc5f661196d13ace4ccc9e143f
Yazima <yazima04200@gmail.com>
2022-09-27 17:34:16 +0900

    ボタン切り替えとカウント機能追加

--- src/components/App.js
+++ src/components/App.js
@@ -4,8 +4,23 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {name: 'にんじゃわんこ'};
+    {/*
+    this.state = {count: 0};
+    */}
+
+
+  }
+
+  handleClick(name) {
+    this.setState({name: name});
     
   }
+ 
+  {/*
+  handleClick() {
+  this.setState({count: this.state.count + 1});
+  }
+  */}
 
   render() {
     const name = 'にんじゃわんこ';
@@ -14,14 +29,23 @@ class App extends React.Component {
     return (
       <div>
         <h1>{name}</h1>
-        <h1>Hello World</h1>
+        <h1 className='title'>Hello World</h1>
         <p>一緒にReactを学びましょう！</p>
 
         <img src={imgUrl} />
 
         <h1>こんにちは、{this.state.name}さん！</h1>
-        <button onClick={() => {this.setState({name: 'ひつじ仙人'})}}>ひつじ仙人</button>
-        <button onClick={() => {this.setState({name: 'にんじゃわんこ'})}}>にんじゃわんこ</button>
+        <button onClick={() => {this.handleClick('ひつじ仙人')}}>ひつじ仙人</button>
+        <button onClick={() => {this.handleClick('にんじゃわんこ')}}>にんじゃわんこ</button>
+
+      {/*
+      <h1>
+        {this.state.count}
+      </h1>
+      <button onClick={()=>{this.handleClick()}}>
+        +
+      </button>
+      */}
 
       </div>
     );
